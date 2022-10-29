9c31fc7e051ad16f1b513a7e87682ed2edf351b3
Emil Eriksson <emil.eriksson@codemill.se>
2022-10-29 23:33:59 +0200

    Add solution for Day 10 B

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,3 +1,5 @@
+use std::cmp::Reverse;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -27,6 +29,29 @@ fn adapter_difference_count(adapters: &[i32]) -> (i32, i32) {
     (ones, threes)
 }
 
+/// Part B: Count the number of ways the adapters can be combined
+fn number_of_identical_chains(adapters: &[i32]) -> u64 {
+    let mut numbers = vec![0; adapters.len()];
+    numbers.copy_from_slice(adapters);
+    numbers.sort_by_key(|&w| Reverse(w));
+
+    let mut cache: HashMap<i32, u64> = HashMap::new();
+    // Prepopulate cache with the possible solutions for the last adapter.
+    cache.insert(numbers[0], 1);
+
+    numbers[1..].iter()
+        .for_each(|&a| {
+            // Find all combinations we can reach that are in the cache
+            let sum = (a..=(a+3)).filter_map(|k| cache.get(&k)).sum();
+            //eprintln!("{:>3}:{}", a, sum);
+            cache.insert(a, sum);
+        });
+
+    let combinations = (0..=3).filter_map(|k| cache.get(&k)).map(|&v| v).sum();
+
+    combinations
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
@@ -42,4 +67,8 @@ fn main() {
     // Part A
     let (ones, threes) = adapter_difference_count(&numbers[..]);
     println!("Day 10 A: {} x {} = {}", ones, threes, ones * threes);
+
+    // Part B
+    let combinations = number_of_identical_chains(&numbers[..]);
+    println!("Day 10 B: {}", combinations);
 }
